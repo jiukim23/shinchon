3891489b07dc54be2d2c89a2aa815928b3cfd33e
shinchon_jiu <hijiuuuu@gmail.com>
2025-04-30 18:18:07 +0900

    adding mobile

--- gongcha/js/mobilemenu.js
+++ gongcha/js/mobilemenu.js
@@ -1,25 +1,25 @@
 window.addEventListener('load',()=>{
-    const header=document.querySelector('header')
-    const menuwrap=document.querySelector('#mobilemenu')
+    // const header=document.querySelector('header')
     const menubtn=document.querySelector('.mobilemenu_btn')
+    const menuwrap=document.querySelector('#mobilemenu')
     const menuLi=document.querySelectorAll('.mobilemenu_list>li')
 
     let menuWrapWidth=window.innerWidth
     let menuHeight=null
     let isOpen=false
-    let isActive=null
-
+    let isActive=false
 
     // 메뉴랩 초기설정
     gsap.set(menuwrap,{right:-menuWrapWidth})
 
+    menubtn.style.border='solid 2px red'
     menubtn.addEventListener('click',mobileMenuOpen)
     menubtn.addEventListener('focus',mobileMenuOpen)
 
     for (const item of menuLi){
         const menuTitle=item.querySelector('button')
         menuTitle.addEventListener('click',()=>{subMenuOpen(item, menuTitle)})
-        memiTitle.addEventListener('focus',()=>{subMenuOpen(item,menuTitle)})
+        menuTitle.addEventListener('focus',()=>{subMenuOpen(item,menuTitle)})
     }
 
     function mobileMenuOpen(){
@@ -27,8 +27,9 @@ window.addEventListener('load',()=>{
             menubtn.style.backgroundImage = "url('/gongcha/images/mobilemenu_x.svg')";
             gsap.set('body,html',{overflow:'hidden'})
             gsap.set(menuwrap, {display:'block'})
-            gsap.to(menuwrap,{right:0,duration:0.2,ease:'power1.out'})
-            isOpen=true
+            gsap.to(menuwrap,{right:0,duration:0.2,ease:'power1.out', onComplete:()=>{
+                isOpen=true
+            }})
         }else if(isOpen == true){
             menubtn.style.backgroundImage="url('/gongcha/images/mobilemenu_btn.svg')"
             gsap.to(menuwrap, {right:-menuWrapWidth,duration:0.2,ease:'power1.out', onComplete:()=>{
@@ -43,28 +44,51 @@ window.addEventListener('load',()=>{
     }
 
     function subMenuOpen(item, menuTitle){
-        isActive=item.classList.contains('active')
-
-        if(!isActive){
+        if(!item.classList.contains('active')){
             activeSubMenu(item, menuTitle)
-
-        }else if(isActive){
+        }else{
             unActiveSubMenu(item, menuTitle)
         }
     }
 
+    // 모바일서브메뉴 ul의 길이조절방식
     function activeSubMenu(item, menuTitle){
-        item.classList.add('active')
-        menuHeight=item.scrollHeight;
-        gsap.set(item,{height:60})
-        gsap.to(item,{height:menuHeight,duration:0.3,ease:'power1.out'})
-        menuTitle.setAttribute('aria-expanded', 'true')
+        const submenu=item.querySelector('.mobilesubmenu_list');
+        submenu.style.height = 'auto';
+        const submenuHeight=submenu.scrollHeight;
+
+        item.classList.add('open')
+        gsap.set(submenu,{height:0,overflow:'hidden'})
+        gsap.to(submenu, {height:submenuHeight, duration:0.3, ease:'power1.out', onComplete:()=>{
+            item.classList.add('active')
+            menuTitle.setAttribute('aria-expanded', 'true')
+        }})
 
     }
     function unActiveSubMenu(item, menuTitle){
-        gsap.to(item,{height:60,duration:0.3,ease:'power1.out',onComplete:()=>{
+        const submenu=item.querySelector('.mobilesubmenu_list');
+        gsap.to(submenu, {height:0, duration:0.3, ease:'power1.out', onComplete:()=>{
             item.classList.remove('active')
-            menuTitle.setAttribute('aria-expanded', 'false')
+            menuTitle.setAttribute('aria-expanded','false')
         }})
     }
+
+
+    // 모바일메뉴의 li가 열리는 방식
+
+    // function activeSubMenu(item, menuTitle){
+    //     menuHeight=item.scrollHeight;
+    //     gsap.set(item,{height:60})
+    //     gsap.to(item,{height:menuHeight,duration:0.3,ease:'power1.out',onComplete:()=>{
+    //         item.classList.add('active')
+    //         menuTitle.setAttribute('aria-expanded', 'true')
+    //     }})
+    // }
+    // function unActiveSubMenu(item, menuTitle){
+    //     gsap.to(item,{height:60,duration:0.3,ease:'power1.out',onComplete:()=>{
+    //         item.classList.remove('active')
+    //         menuTitle.setAttribute('aria-expanded', 'false')
+    //     }})
+    // }
+
 })
---
+++ gongcha/js/mobileswiper.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    var swiper = new Swiper(".mobile_swiper", {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 20,
+        pagination: {
+          el: ".swiper-pagination",
+          type: "custom",
+          renderCustom: function (swiper, current, total) {
+            return `
+            <div class="custom_fraction">
+                <img src="/gongcha/images/fraction_left.svg" class="fraction_icon">
+                <span class="pagination-text">
+                    ${current} / ${total}
+                </span>
+                <img src="/gongcha/images/fraction_right.svg" class="fraction_icon">
+            </div>
+            `;
+          },
+        },
+      });
+})
